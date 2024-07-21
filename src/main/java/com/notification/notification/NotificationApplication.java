b68ec208db36023af1d45703d42219aa4435f32d
Ajay Mishra <ajaymishra.adt@gmail.com>
2024-07-21 14:54:31 +0530

    consumer config

--- src/main/java/com/notification/notification/NotificationApplication.java
+++ src/main/java/com/notification/notification/NotificationApplication.java
@@ -1,13 +1,12 @@
-package com.sendNotification;
+package com.notification.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SendNotificationApplication {
+public class NotificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SendNotificationApplication.class, args);
+		SpringApplication.run(NotificationApplication.class, args);
 	}
-
 }
--- src/test/java/com/notification/notification/NotificationApplicationTests.java
+++ src/test/java/com/notification/notification/NotificationApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sendNotification;
+package com.notification.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SendNotificationApplicationTests {
+class NotificationApplicationTests {
 
 	@Test
 	void contextLoads() {
